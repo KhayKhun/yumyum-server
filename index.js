6eab5c6bb1da0836eac56d0824233a74a5376655
KhayKhun <khaykhun22052006@gmail.com>
2023-10-27 14:57:23 +0630

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const httpServer = require('http').createServer(app);
+const { Server } = require("socket.io");
+
+const io = new Server(httpServer,{
+  cors : {
+    origin : ['http://localhost:5173','http://localhost:5174']
+  }
+});
+
+app.get('/', (req, res) => {
+  res.send('Hi mom!')
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected => '+socket.id);
+
+  socket.on('join', id => {
+    socket.join(id);
+    console.log('joined --'+ id)
+  })
+
+  socket.on('order-from-customer', (order)=>{
+    console.log('from:' +  order.customer_id + ' to:'+order.seller_id, order);
+    socket.join(order.customer_id);
+    io.to(order.customer_id).emit('nice','Placed order');
+    io.to(order.seller_id).emit('server:order-from-client', order)
+  })
+
+  socket.on('approve',(order) => {
+    console.log('approved by resturant')
+    console.log(order)
+    io.to(order.customer_id).emit('approved', order)
+  })
+  socket.on('reject',(order) => {
+    console.log('rejected by resturant')
+    console.log(order)
+    io.to(order.customer_id).emit('rejected', order)
+  })
+});
+
+httpServer.listen(8000, () => {
+  console.log('listening on *:8000');
+});
